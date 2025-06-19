3800ac533aa49c716b3f6da5dbfe1ecd9ff13679
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2025-06-19 23:50:19 +0530

    Dynamic Routes and Catch all segments

---
+++ app/blog/[postId]/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog({params} : any) {
+
+    const postId = params.postId;
+    return <div>
+        Blog {postId}
+    </div>
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+
 export default function Signup() {
-    return <div className="h-screen w-screen flex justify-center items-center">
-        <div className="flex flex-col p-4 gap-4 border-r-2 rounded-2xl">
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return <div className="h-screen w-screen flex justify-center items-center p-6">
+        <div className="flex flex-col p-8 gap-4 border-r-2 rounded-2xl border-white">
             <div className="text-3xl text-white">Singup</div>
-            <input type="text" placeholder="Enter username" className="border-2"/>
-            <input type="password" placeholder="Enter password" className="border-2"/>
+            <input type="text" placeholder="Enter username" className="border-2" onChange={(e) => {
+                setUsername(e.target.value)
+            }}/>
+            <input type="password" placeholder="Enter password" className="border-2" onChange={(e) => {
+                setPassword(e.target.value)
+            }}/>
             <button className="w-full bg-amber-300 text-orange-700"
             onClick={() => {
-                
+                axios.post("http://localhost:3000/api/v1/signup", {
+                    username,
+                    password
+                })
             }}
             >Create Account</button>
         </div>
